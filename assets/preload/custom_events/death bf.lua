7869f8c8605f110b8e1c2f4303b4789b84a4886d
Htxgamegh <116314659+Htxgamegh@users.noreply.github.com>
2022-12-07 20:00:39 -0300

    Add files via upload

---
+++ assets/preload/custom_events/death bf.lua
@@ -0,0 +1,9 @@
+function onCreate()
+	setPropertyFromClass('GameOverSubstate', 'characterName', 'bf-pet'); --Character json file for the death animation
+	setPropertyFromClass('GameOverSubstate', 'deathSoundName', 'fnf_mongus_death'); --put in mods/sounds/
+	setPropertyFromClass('GameOverSubstate', 'loopSoundName', 'gameOver'); --put in mods/music/
+	setPropertyFromClass('GameOverSubstate', 'endSoundName', 'gameOverEnd'); --put in mods/music/
+
+
+
+end
